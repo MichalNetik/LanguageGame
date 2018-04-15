3fb17da14ba6525bad0e01d1cd2587ef333325ec
Michal Netik <michal.netik99@gmail.com>
2018-04-15 21:16:28 +0200

    Initial commit - angular project

---
+++ angular/lang-game/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class LangGamePage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
