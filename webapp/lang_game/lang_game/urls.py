a6c868df561f32569e08e5ec7cb1de9e7190cc8a
Michal Netik <michal.netik99@gmail.com>
2018-05-20 08:16:10 +0100

    Front end served from django app

--- webapp/lang_game/lang_game/urls.py
+++ webapp/lang_game/lang_game/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'', index, name='index'),
+    path(r'admin/', admin.site.urls),
+    path(r'^(?P<path>.*)$', index)
 ]
