27c4558e793d1c418fd79d1a112495ea7fe0927f
Michal Netik <michal.netik99@gmail.com>
2018-08-19 13:12:18 +0100

    #5 - auth placeholder

--- angular/lang-game/src/app/modules/top-menu/top-menu.module.ts
+++ angular/lang-game/src/app/modules/top-menu/top-menu.module.ts
@@ -1,9 +1,12 @@
 import { AppRoutingModule } from '../../app-routing.module';
 import { NgModule } from '@angular/core';
 import { TopMenuComponent } from './top-menu.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
 
 @NgModule({
-  declarations: [TopMenuComponent],
+  declarations: [TopMenuComponent, AuthenticationComponent, LoginComponent, SignupComponent],
   imports: [
     AppRoutingModule
   ],
