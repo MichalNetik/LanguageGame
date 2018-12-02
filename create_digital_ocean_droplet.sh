cdc69f26c2f4981b5cb2e9cf6b66af0f77593e6d
michal <michal.netik99@gmail.com>
2018-12-02 19:53:58 +0000

    #2: trying to create a digital ocean droplet on fly - part 12

--- create_digital_ocean_droplet.sh
+++ create_digital_ocean_droplet.sh
@@ -1,10 +1,10 @@
 SSH_KEY_FINGERPRINT=$1
 BEARER_TOKEN=$2
 
-DROPLET_DETAILS=curl -X POST "https://api.digitalocean.com/v2/droplets" \
+DROPLET_DETAILS=$(curl -X POST "https://api.digitalocean.com/v2/droplets" \
     -d '{"name":"langgame-dev","region":"fra1","size":"s-1vcpu-1gb","image":"docker-18-04","ssh_keys":["$SSH_KEY_FINGERPRINT"]}' \
     -H "Authorization: Bearer $BEARER_TOKEN" \
-    -H "Content-Type: application/json"
+    -H "Content-Type: application/json")
 
 echo $DROPLET_DETAILS
 
