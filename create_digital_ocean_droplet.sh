dd203a05d5f8c17ed39ce6316472d0652b8c1583
michal <michal.netik99@gmail.com>
2018-12-02 20:56:35 +0000

    #2: get details(ip address) for a newly created droplet - 6

--- create_digital_ocean_droplet.sh
+++ create_digital_ocean_droplet.sh
@@ -1,8 +1,12 @@
 SSH_KEY_FINGERPRINT=$1
 BEARER_TOKEN=$2
 
+CREATE_DROPLET_JSON='{"name":"langgame-dev","region":"fra1","size":"s-1vcpu-1gb","image":"docker-18-04","ssh_keys":['"$SSH_KEY_FINGERPRINT"']}'
+echo "JSON to create a droplet: $CREATE_DROPLET_JSON"
+
+
 DROPLET_DETAILS=$(curl -X POST "https://api.digitalocean.com/v2/droplets" \
-    -d '{"name":"langgame-dev","region":"fra1","size":"s-1vcpu-1gb","image":"docker-18-04","ssh_keys":['"$SSH_KEY_FINGERPRINT"']}' \
+    -d $CREATE_DROPLET_JSON \
     -H "Authorization: Bearer $BEARER_TOKEN" \
     -H "Content-Type: application/json")
 
