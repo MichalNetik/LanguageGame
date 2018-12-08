a8b18663dea66afa24d65e55671943327f01fadc
michal <michal.netik99@gmail.com>
2018-12-08 14:35:05 +0000

    #2: push images to docker hub - 7

--- bin/delete_digital_ocean_droplet.sh
+++ bin/delete_digital_ocean_droplet.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-for i in {i: 8}
-do
-    echo "Waiting to delete the dev droplet "
-    sleep 30s
-done
-
 DELETE_STATUS=$(curl -X DELETE "https://api.digitalocean.com/v2/droplets/$NEW_DROPLET_ID" \
     -d $CREATE_DROPLET_JSON \
     -H "Authorization: Bearer $BEARER_TOKEN" \
