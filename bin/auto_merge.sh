1c9971458a0732de60297f6663ce7046449a4012
michal <michal.netik99@gmail.com>
2018-12-15 13:34:26 +0000

    #2: intial version of automatic merge into master

---
+++ bin/auto_merge.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+FROM_BRANCH=$1
+TO_BRANCH=$2
+GITHUB_TOKEN=$3
+
+GITHUB_REPO="MichalNetik/LanguageGame"
+
+
+REPO_TEMP=$(mktemp -d)
+git clone "https://github.com/$GITHUB_REPO" "$REPO_TEMP"
+
+pushd $REPO_TEMP
+
+git checkout "$TO_BRANCH"
+git merge --ff-only "$FROM_BRANCH"
+
+
+PUSH_URI="https://$GITHUB_TOKEN@github.com/$GITHUB_REPO"
+
+git push "$PUSH_URI" "$TO_BRANCH" >/dev/null 2>&1
+
+popd
+
+
+
