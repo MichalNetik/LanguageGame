1c36fa53825c0046ce76f45eb1b193dbd92f6a01
michal <michal.netik99@gmail.com>
2019-01-27 11:55:21 +0000

    #20 - possibility to store data by user + all connected functionality, new data fixtures, fixed admin for local usage

--- webapp/core/serializers/vocabulary_category_serializer.py
+++ webapp/core/serializers/vocabulary_category_serializer.py
@@ -7,5 +7,10 @@ class VocabularyCategorySerializer(serializers.ModelSerializer):
         model = VocabularyCategory
         fields = '__all__'
         extra_kwargs = {
-            'id': {'read_only': False, 'required': False, 'allow_null': True}
+            'id': {'read_only': False, 'required': False, 'allow_null': True},
+            'user': {'read_only': False, 'required': False}
         }
+
+    def create(self, validated_data):
+        user = self.context.get("request").user
+        return VocabularyCategory.objects.create(user=user, **validated_data)
