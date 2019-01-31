62825e203673448f2fbdf9b42b43c52c71efe317
michal <michal.netik99@gmail.com>
2019-01-31 18:19:16 +0000

    #26 - created unittests for getSortIcon

--- angular/src/app/shared/models/specs/pagination-url-params.spec.ts
+++ angular/src/app/shared/models/specs/pagination-url-params.spec.ts
@@ -1,14 +1,31 @@
 import { PaginationUrlParamsModel } from './../pagination-url-params.model';
 
 describe('Pagination Url Params', () => {
-  it('#getEmpty should create an empty instance', () => {
-    const item = PaginationUrlParamsModel.getEmpty('testColumn');
+  let model: PaginationUrlParamsModel;
 
-    expect(item.startOffset).toBe(0);
-    expect(item.endOffset).toBe(10);
-    expect(item.sortColumn).toBe('testColumn');
-    expect(item.sortDirection).toBe('asc');
-    expect(item.pageSize).toBe(10);
-    expect(item.totalRecords).toBe(0);
+  beforeEach(() => {
+    model = PaginationUrlParamsModel.getEmpty('testColumn')
+  })
+
+  it('#getEmpty should create an empty instance', () => {
+    expect(model.startOffset).toBe(0);
+    expect(model.endOffset).toBe(10);
+    expect(model.sortColumn).toBe('testColumn');
+    expect(model.sortDirection).toBe('asc');
+    expect(model.pageSize).toBe(10);
+    expect(model.totalRecords).toBe(0);
+  });
+
+  it('#getSortIcon should return "sort-up" for the current sort column', () => {
+    expect(model.getSortIcon('testColumn')).toBe('sort-up');
+  });
+
+  it('#getSortIcon should return "sort-down" for the current sort column', () => {
+    model.sortDirection = 'desc';
+    expect(model.getSortIcon('testColumn')).toBe('sort-down');
+  });
+
+  it('#getSortIcon should return "sort" for a non-sort column', () => {
+    expect(model.getSortIcon('anotherTestColumn')).toBe('sort');
   });
 });
