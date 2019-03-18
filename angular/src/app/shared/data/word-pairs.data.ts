ccb55cc8d059751f4df109561205652e82380a74
michal <michal.netik99@gmail.com>
2019-03-18 15:46:06 +0000

    #38: fixed failing lint check

--- angular/src/app/shared/data/word-pairs.data.ts
+++ angular/src/app/shared/data/word-pairs.data.ts
@@ -1,161 +1,161 @@
-export const wordPairsData = {  
-  "data":[  
-     {  
-        "id":1,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+export const wordPairsData = {
+  'data':  [
+     {
+        'id':  1,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"cat",
-        "translated":"kočka",
-        "description":null
+        'base': 'cat',
+        'translated': 'kočka',
+        'description': null
      },
-     {  
-        "id":2,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id':  2,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"dog",
-        "translated":"pes",
-        "description":null
+        'base': 'dog',
+        'translated': 'pes',
+        'description': null
      },
-     {  
-        "id":3,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id': 3,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"elephant",
-        "translated":"slon",
-        "description":null
+        'base': 'elephant',
+        'translated': 'slon',
+        'description': null
      },
-     {  
-        "id":4,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id': 4,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"bird",
-        "translated":"pták",
-        "description":null
+        'base': 'bird',
+        'translated': 'pták',
+        'description': null
      },
-     {  
-        "id":6,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id': 6,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"cow",
-        "translated":"kráva",
-        "description":null
+        'base': 'cow',
+        'translated': 'kráva',
+        'description': null
      },
-     {  
-        "id":14,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id':  14,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"squirrel",
-        "translated":"veverka",
-        "description":"little forest animal"
+        'base': 'squirrel',
+        'translated': 'veverka',
+        'description': 'little forest animal'
      },
-     {  
-        "id":15,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id':  15,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"fox",
-        "translated":"liška",
-        "description":null
+        'base': 'fox',
+        'translated': 'liška',
+        'description': null
      },
-     {  
-        "id":17,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id':  17,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"pig",
-        "translated":"prase",
-        "description":null
+        'base': 'pig',
+        'translated': 'prase',
+        'description': null
      },
-     {  
-        "id":18,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id':  18,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"camel",
-        "translated":"velbloud",
-        "description":null
+        'base': 'camel',
+        'translated': 'velbloud',
+        'description': null
      },
-     {  
-        "id":26,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id':  26,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"chicken",
-        "translated":"kuře",
-        "description":null
+        'base': 'chicken',
+        'translated': 'kuře',
+        'description': null
      },
-     {  
-        "id":33,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id': 33,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"rat",
-        "translated":"krysa",
-        "description":null
+        'base': 'rat',
+        'translated': 'krysa',
+        'description': null
      },
-     {  
-        "id":34,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id': 34,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"fish",
-        "translated":"ryba",
-        "description":null
+        'base': 'fish',
+        'translated': 'ryba',
+        'description': null
      },
-     {  
-        "id":35,
-        "category":{  
-           "id":1,
-           "name":"Animals",
-           "description":"Different kinds of animals",
-           "user":1
+     {
+        'id': 35,
+        'category':  {
+           'id':  1,
+           'name': 'Animals',
+           'description': 'Different kinds of animals',
+           'user':  1
         },
-        "base":"monkey",
-        "translated":"opice",
-        "description":null
+        'base': 'monkey',
+        'translated': 'opice',
+        'description': null
      }
   ],
-  "totalRecords":13
+  'totalRecords':  13
 };
